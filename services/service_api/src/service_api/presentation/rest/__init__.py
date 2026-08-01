4ba2c34a361e4607d1534fd689b3c587abe840d0
Nikita Bondarev <nikitabondarevvitebsk@gmail.com>
2026-08-01 22:49:38 +0300

    Module "API Service" - Внесены мелкие изменения

--- services/service_api/src/service_api/presentation/rest/__init__.py
+++
@@ -1,7 +0,0 @@
-from .routers import cabinet_router, group_router, schedule_router
-
-__all__ = [
-    'cabinet_router',
-    'group_router',
-    'schedule_router'
-]
--- services/service_api/src/service_api/presentation/rest/routers.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-
-group_router = APIRouter(prefix='/groups/')
-cabinet_router = APIRouter(prefix='/cabinets/')
-schedule_router = APIRouter(prefix='/schedule/')
