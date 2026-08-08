7013fa49f89cd5a9be2e7a0c68ccd785132e44c7
Nikita Bondarev <nikitabondarevvitebsk@gmail.com>
2026-08-08 21:33:53 +0300

    Microservice "Bot Service" - Микросервис реструктуризирован в пользу уменьшения нецелесообразной вложенности файлов - Файл с ошибками разбит на несколько файлов с разделением по категориям ошибок - Добавлена конфигурация системы (BaseSystemSettings) - Уточнено содержание bot_settings.env.example - Интегрированы логи системы JSON формата - Добавлены контекстные переменные для целей логирования - Добавлены фильтры для автоматической интеграции контекстных переменных в логи - Большая часть кода системы, включая бизнес-логику, покрыта тестами - Конфигурации бота, БД и Redis настроены под интеграцию с HashiCorp Vault - Добавлены менеджеры БД (DatabaseEngineManager) и Redis (RedisClientManager) - Клиент HTTPX настроен под API микросервиса "API Service" - Репозитории, взаимодействующие с внешними сервисами через HTTP, были пересмотрены в части формировании ссылки и обработки ошибки 404 - Добавлен middleware InitRequestMiddleware для логирования начала и окончания выполнения запроса - Добавлен шаблон dictConfig логирования - Добавлены настройки логирования - Добавлены обработчики on_startup и on_shutdown - Добавлена система "горячей" замены сертификатов и пересоздания соединений с БД и Redis (на базе watchfiles) - Реализованы тесты репозиториев httpx_group_repository.py, httpx_cabinet_repository.py, httpx_schedule_repository.py - Реализована часть тестов репозиториев sqlalchemy_user_repository.py - Внесены мелкие изменения

--- services/service_bot/src/service_bot/infrastructure/middlewares/delete_message.py
+++ services/service_bot/src/service_bot/infrastructure/middlewares/delete_message.py
@@ -1,12 +1,17 @@
+import logging
 from collections.abc import Awaitable, Callable
 from typing import Any
 
 from aiogram import BaseMiddleware
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import Message
+
+logger = logging.getLogger(__name__)
 
 
 class DeleteMessageMiddleware(BaseMiddleware):
     """Middleware удаления текстового сообщения"""
+
     async def __call__(
             self,
             handler: Callable[[Message, dict[str, Any]], Awaitable[Any]],
@@ -14,8 +19,10 @@ class DeleteMessageMiddleware(BaseMiddleware):
             data: dict[str, Any]
     ):
         try:
+            logger.info('Deleting the original message')
             await event.delete()
-        except:
-            pass
+            logger.info('The original message has been successfully deleted')
+        except TelegramBadRequest:
+            logger.exception('Error while deleting the original message')
 
         await handler(event, data)
